5f201e02072579400c9249e53b21dd1833e41770
spamsgood <spamsgood@gmail.com>
2024-01-28 23:58:22 -0800

    typo fix

--- src/DeleteAllData.java
+++ src/DeleteAllData.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.util.Scanner; // Import statement for Scanner
+import java.util.Scanner;
 
 public class DeleteAllData {
 
